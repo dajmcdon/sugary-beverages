50f17c0a108c1bb40199c58fc3f76dadf856426c
parpishro <par.pishrobat@gmail.com>
2024-03-01 18:45:15 -0800

    finished the plotting functions and made them consistent and color-blind friendly.

--- code/EDA.R
+++ code/EDA.R
@@ -12,55 +12,67 @@ summarize_data <- function(df) {
 
 #' Plot Histograms for Numeric Variables
 #'
-#' Plots histograms for all numeric variables in the dataset to explore distributions.
-#' @param df Data frame containing numeric variables.
-plot_histograms <- function(df) {
+#' Plots histograms for specified numeric variables in the dataset to explore distributions.
+#'
+#' @param df        Data frame containing numeric variables.
+#' @param var_names Names of the variables to be plotted.
+plot_histograms <- function(df, var_names) {
   numeric_vars <- select_if(df, is.numeric) %>% names()
   
-  for (var in numeric_vars) {
-    print(ggplot(df, aes_string(x = var)) + 
-      geom_histogram(bins = 20, fill = "blue", color = "black", na.rm = T) + 
-      theme_minimal() + 
-      labs(title = paste("Distribution of", var)))
+  if (!all(var_names %in% numeric_vars)) {
+    stop("All variables in 'var_names' must be numeric.")
   }
+  
+  df_long <- df %>%
+    pivot_longer(cols = var_names, names_to = "Variables", values_to = "Sales") %>%
+    mutate(Variables = factor(Variables, levels = var_names))
+  
+  p <- ggplot(df_long, aes(x = Sales, fill = Variables)) + 
+    geom_histogram(bins = 20, color = "black", na.rm = TRUE) +
+    scale_fill_viridis(discrete = TRUE) +
+    theme_minimal() +
+    facet_wrap(~Variables, scales = "free", ncol = length(var_names)) + 
+    theme(legend.position = "none") +
+    labs(title = "Distribution of Selected Variables")
+  
+  print(p)
 }
+
+
 
 
 #' Boxplots for Numeric Variables by Category
 #'
 #' Generates boxplots for numeric variables by a specified categorical variable.
-#' @param df Data frame containing the data.
-#' @param category Name of the categorical variable.
-boxplots_by_category <- function(df, category) {
-  numeric_vars <- select_if(df, is.numeric) %>% names()
-  
-  for (var in numeric_vars) {
-    print(ggplot(df, aes_string(x = category, y = var, fill = category)) + 
-      geom_boxplot(na.rm = T) + 
-      theme_minimal() + 
-      labs(title = paste("Boxplot of", var, "by", category)))
-  }
-}
-
-
-#' Scatter Plots for Numeric Variables
 #'
-#' Generates scatter plots for pairs of numeric variables.
 #' @param df Data frame containing the data.
-scatter_plots <- function(df) {
+#' @param responses the variables to be plotted as response in boxplots
+#' @param category Name of the categorical variable.
+boxplots_by_category <- function(df, responses, category) {
   numeric_vars <- select_if(df, is.numeric) %>% names()
   
-  if (length(numeric_vars) > 1) {
-    for (i in 1:(length(numeric_vars) - 1)) {
-      for (j in (i + 1):length(numeric_vars)) {
-        print(ggplot(df, aes_string(x = numeric_vars[i], y = numeric_vars[j])) +
-          geom_point(alpha = 0.5, na.rm = T) +
-          theme_minimal() +
-          labs(title = paste("Scatter Plot of", numeric_vars[i], "vs", numeric_vars[j])))
-      }
-    }
+  if (!all(responses %in% numeric_vars)) {
+    stop("All variables in 'responses' must be numeric.")
   }
+  
+  # Convert the category to a factor based on its original order in the dataframe
+  df[[category]] <- factor(df[[category]], levels = unique(df[[category]]))
+  
+  # Create a long format data frame suitable for faceting
+  df_long <- df %>%
+    pivot_longer(cols = responses, names_to = "Response", values_to = "Sales")
+  
+  p <- ggplot(df_long, aes(x = !!sym(category), y = Sales, fill = !!sym(category))) + 
+    geom_boxplot(na.rm = TRUE) +
+    scale_fill_viridis(discrete = TRUE) +
+    theme_minimal() +
+    facet_wrap(~Response, scales = "free_y", nrow = length(responses)) + 
+    theme(legend.position = "bottom") + 
+    labs(title = paste("Boxplot of responses by", category))
+  
+  print(p)
 }
+
 
 
 #' Summarize Sales by Dynamic Category
@@ -95,22 +107,59 @@ handle_missing_data <- function(df) {
 #' Plot Scatter for Zero-Calorie vs Sugary Beverage Sales
 #'
 #' Creates a scatter plot to visualize the relationship between zero-calorie and sugary beverage sales, colored by site.
+#'
 #' @param df Data frame containing the beverage sales data.
 #' @param x_var Name of the variable representing zero-calorie beverage sales.
 #' @param y_var Name of the variable representing sugary beverage sales.
 #' @param color_var Name of the variable to be used for point colors, typically site.
-#' @param title Plot title.
-#' @param x_lab Label for the x-axis.
-#' @param y_lab Label for the y-axis.
+#'
 #' @return A ggplot object representing the scatter plot.
-plot_beverage_sales_comparison <- function(df, x_var, y_var, color_var, title = "Comparison of Zero-Calorie and Sugary Beverage Sales",
-                                           x_lab = "Zero-Calorie Beverage Sales", y_lab = "Sugary Beverage Sales") {
-  ggplot(df, aes_string(x = x_var, y = y_var, color = color_var)) +
-    geom_point() +
+plot_beverage_sales_comparison <- function(df, x_var, y_var, color_var) {
+  df <- df %>%
+    mutate(!!color_var := factor(!!sym(color_var), levels = unique(df[[color_var]]))) %>%
+    drop_na(!!sym(x_var), !!sym(y_var), !!sym(color_var))
+  
+  p <- ggplot(df, aes(x = !!sym(x_var), y = !!sym(y_var), color = !!sym(color_var))) + 
+    geom_point(alpha = 0.5, na.rm = T) +
+    geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "black") +
+    scale_color_viridis(discrete = TRUE) +
     theme_minimal() +
-    labs(title = title, x = x_lab, y = y_lab)
+    labs(title = paste("Comparison of", x_var, "and", y_var, "Sales"),
+         x = x_var, 
+         y = y_var)
+  
+  print(p)
 }
 
+
+#' Plot Time Series of Beverage Sales
+#'
+#' Creates a line plot to visualize the sales of zero-calorie and sugary beverages over time,
+#' differentiated by site. Marks the start of each intervention with a dashed line and label.
+#'
+#' @param df Data frame containing the sales data.
+plot_sales_time_series <- function(df) {
+  interventions <- unique(df$Intervention)
+  intervention_starts <- sapply(interventions, function(x) which(df$Intervention == x)[1])
+  intervention_df <- data.frame(start = intervention_starts, label = interventions)
+  intervention_df$label <- c("", "dismes", "", "dis", "", "follow", "cal", "exer", "both")
+  
+  df$Site <- factor(df$Site, levels = unique(df$Site))
+  
+  p <- ggplot(df, aes(x = Count)) +
+    geom_line(aes(y = ZeroCal, color = "ZeroCal"), na.rm = TRUE) +
+    geom_line(aes(y = Sugary, color = "Sugary"), na.rm = TRUE) +
+    scale_color_viridis(discrete = TRUE, begin = 0.2, end = 0.8) +
+    geom_vline(data = intervention_df, aes(xintercept = start), linetype = "dashed") +
+    geom_text(data = intervention_df, aes(x = start+ 10, y = 0, label = label), vjust = -5.5) +
+    facet_wrap(~Site, ncol = 1, scales = "free_y") +
+    theme_minimal() +
+    labs(title = "Sales Over Time by Site", x = "Day", y = "Sales", color = "Beverage Type") +
+    theme(legend.position = "top") +
+    scale_y_continuous(expand = expansion(mult = c(0.05, 0.25)))
+  
+  print(p)
+}
 
 
 
